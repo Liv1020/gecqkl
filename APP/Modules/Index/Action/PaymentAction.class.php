3d5142a54f3abb2149062cbddb554d28f2197d5c
niudongdong <956778666@qq.com>
2019-01-16 18:26:12 +0800

    update

--- APP/Modules/Index/Action/PaymentAction.class.php
+++ APP/Modules/Index/Action/PaymentAction.class.php
@@ -106,12 +106,12 @@ Class PaymentAction extends CommonAction {
         $body = "开通钱包";//内容
         $total_fee = $money; //金额
         $spbill_create_ip = $userip; //IP
-        //$notify_url = "http://cs.gsxswl.com/index.php/index/payment/h5_notifys"; //回调地址
+        //$notify_url = "http://www.zgllsj.com/index.php/index/payment/h5_notifys"; //回调地址
         $notify_url = "http://www.zgllsj.com/Api/weixinh5/qianbao_url.php";
-        //$notify_url="http://cs.gsxswl.com/index.php/Index/Payment/h5_notifys";
+        //$notify_url="http://www.zgllsj.com/index.php/Index/Payment/h5_notifys";
 
         $trade_type = 'MWEB';//交易类型 具体看API 里面有详细介绍
-        $scene_info ='{"h5_info":{"type":"Wap","wap_url":"http://cs.gsxswl.com","wap_name":"微信支付"}}';//场景信息 必要参数
+        $scene_info ='{"h5_info":{"type":"Wap","wap_url":"http://www.zgllsj.com","wap_name":"微信支付"}}';//场景信息 必要参数
         $signA ="appid=$appid&attach=$out_trade_no&body=$body&mch_id=$mch_id&nonce_str=$nonce_str&notify_url=$notify_url&out_trade_no=$out_trade_no&scene_info=$scene_info&spbill_create_ip=$spbill_create_ip&total_fee=$total_fee&trade_type=$trade_type";
         $strSignTmp = $signA . "&key=$key"; //拼接字符串  注意顺序微信有个测试网址 顺序按照他的来 直接点下面的校正测试 包括下面XML  是否正确
         
