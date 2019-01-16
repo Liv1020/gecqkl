e80436c9dcdfa252439bad84bf746e04b530b9ed
niudongdong <956778666@qq.com>
2019-01-16 18:25:43 +0800

    update

--- APP/Modules/Index/Action/PaymentAction.class.php
+++ APP/Modules/Index/Action/PaymentAction.class.php
@@ -88,7 +88,57 @@ Class PaymentAction extends CommonAction {
         echo "<script>location.href='".$objectxml['mweb_url']."&redirect_url=".$nto_html."'</script>";
         //header($objectxml['mweb_url']);
 	}
-	
+	/**
+	 * 开通钱包支付
+	 * Enter description here ...
+	 */
+	public function ktqianbao(){
+		$userip = $this->get_client_ip();     //获得用户设备IP
+        $appid  = "wx9a6756a449493e94";                  //应用APPID
+        $mch_id = "1464752502";                  //微信支付商户号
+        $key    = "xswl3f5246bf0145d835d503282b6413";                 //微信商户API密钥
+        
+        $data = $_POST;
+        $out_trade_no = $_SESSION['username'];;//$_SESSION['username'];//平台内部订单号
+       // $data['total_amount']=floatval($data['total_amount']);
+        $money= 0.01*100;                     //充值金额 微信支付单位为分
+        $nonce_str = $this->createNoncestr(); //随机字符串
+        $body = "开通钱包";//内容
+        $total_fee = $money; //金额
+        $spbill_create_ip = $userip; //IP
+        //$notify_url = "http://cs.gsxswl.com/index.php/index/payment/h5_notifys"; //回调地址
+        $notify_url = "http://www.zgllsj.com/Api/weixinh5/qianbao_url.php";
+        //$notify_url="http://cs.gsxswl.com/index.php/Index/Payment/h5_notifys";
+
+        $trade_type = 'MWEB';//交易类型 具体看API 里面有详细介绍
+        $scene_info ='{"h5_info":{"type":"Wap","wap_url":"http://cs.gsxswl.com","wap_name":"微信支付"}}';//场景信息 必要参数
+        $signA ="appid=$appid&attach=$out_trade_no&body=$body&mch_id=$mch_id&nonce_str=$nonce_str&notify_url=$notify_url&out_trade_no=$out_trade_no&scene_info=$scene_info&spbill_create_ip=$spbill_create_ip&total_fee=$total_fee&trade_type=$trade_type";
+        $strSignTmp = $signA . "&key=$key"; //拼接字符串  注意顺序微信有个测试网址 顺序按照他的来 直接点下面的校正测试 包括下面XML  是否正确
+        
+        $sign = strtoupper(MD5($strSignTmp)); // MD5 后转换成大写
+        
+        $post_data = "<xml>
+                    <appid>$appid</appid>
+                    <mch_id>$mch_id</mch_id>
+                    <body>$body</body>
+                    <out_trade_no>$out_trade_no</out_trade_no>
+                    <total_fee>$total_fee</total_fee>
+                    <spbill_create_ip>$spbill_create_ip</spbill_create_ip>
+                    <notify_url>$notify_url</notify_url>
+                    <trade_type>$trade_type</trade_type>
+                    <scene_info>$scene_info</scene_info>
+                    <attach>$out_trade_no</attach>
+                    <nonce_str>$nonce_str</nonce_str>
+                    <sign>$sign</sign>
+            </xml>";//拼接成XML 格式
+        $nto_html = urlencode("http://www.zgllsj.com/index.php/Index/Payment/qianbao/id/".$out_trade_no);
+        $url = "https://api.mch.weixin.qq.com/pay/unifiedorder";//微信传参地址
+        $dataxml = $this->postXmlCurl($post_data,$url); //后台POST微信传参地址  同时取得微信返回的参数
+       // print_r($dataxml);exit;
+        
+        $objectxml = (array)simplexml_load_string($dataxml, 'SimpleXMLElement', LIBXML_NOCDATA); //将微信返回的XML 转换成数组
+        echo "<script>location.href='".$objectxml['mweb_url']."&redirect_url=".$nto_html."'</script>";
+	}
     public function h5wxpays(){
     	/*$orders = M("orders");
     	$id = $_GET['id'];
