54d61a7b815309da962e650cf6411de691beb53e
niudongdong <956778666@qq.com>
2019-01-23 16:38:57 +0800

    update

--- APP/Modules/Index/Action/SemAction.class.php
+++ APP/Modules/Index/Action/SemAction.class.php
@@ -85,7 +85,8 @@
 		//注册推广
 		 public function regSem(){
 			 header("Content-type:text/html;charset=utf-8");
-			 
+			 $wxcs  = file_get_contents("php://input");
+			 print_r($wxcs);
 			 $d_key=I('get.u','','trim');//$d_keyid=encrypt("t24GWvVczWju",'D','xyb8888');
 			 
 			 if(!is_int($d_key)){
