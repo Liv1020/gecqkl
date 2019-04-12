2537cd62a55c825f0749be3e9c498fff8a34d09e
niudongdong <956778666@qq.com>
2019-04-12 18:39:11 +0800

    update

--- Api/tcp/tcp.php
+++ Api/tcp/tcp.php
@@ -1,101 +1,82 @@
 <?php
-//确保在连接客户端时不会超时  
-/*set_time_limit(0);  
-//设置IP和端口号  
-$address = "39.106.161.182";  
-$port = 3399; 
-$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("socket_create() fail:" . socket_strerror(socket_last_error()) . "/n");  
-//阻塞模式  
-socket_set_block($sock) or die("socket_set_block() fail:" . socket_strerror(socket_last_error()) . "/n");  
-//绑定到socket端口  
-$result = socket_bind($sock, $address, $port) or die("socket_bind() fail:" . socket_strerror(socket_last_error()) . "/n");  
-//开始监听  
-$result = socket_listen($sock, 4) or die("socket_listen() fail:" . socket_strerror(socket_last_error()) . "/n");  
-echo "OK\nBinding the socket on $address:$port ... ";  
-echo "OK\nNow ready to accept connections.\nListening on the socket ... \n";  
-do { // never stop the daemon  
-    //它接收连接请求并调用一个子连接Socket来处理客户端和服务器间的信息  
-    $msgsock = socket_accept($sock) or  die("socket_accept() failed: reason: " . socket_strerror(socket_last_error()) . "/n");  
-    while(1){
-		//读取客户端数据  
-		echo "Read client data \n";  
-		//socket_read函数会一直读取客户端数据,直到遇见\n,\t或者\0字符.PHP脚本把这写字符看做是输入的结束符.  
-		$buf = socket_read($msgsock, 8192);  
-		echo "Received msg: $buf   \n";
- 
-		if($buf == "bye"){
-			//接收到结束消息，关闭连接，等待下一个连接
-			socket_close($msgsock);
-			continue;
-		}
-		  
-		//数据传送 向客户端写入返回结果  
-		$msg = "welcome \n";  
-		socket_write($msgsock, $msg, strlen($msg)) or die("socket_write() failed: reason: " . socket_strerror(socket_last_error()) ."/n");  		
-	}  
-      
-} while (true);  
-socket_close($sock);  */
+/**
+ * @description HttpServer类
+ * @author luoluolzb <luoluolzb@163.com>
+ * @date   2018/3/2
+ */
+class HttpServer
+{
+    protected $port;
+    protected $address;
+    protected $socket;
 
-	// Server
-// 设置错误处理
-error_reporting(E_ALL);
-// 设置运行时间
-set_time_limit(0);
-// 起用缓冲
-ob_implicit_flush();
-$ip = "39.106.161.182"; // IP地址
-$port = 8080; // 端口号
- 
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP); // 创建一个SOCKET
-if ($socket)
-    echo "socket_create() successed!\n";
-else
-    echo "socket_create() failed:".socket_strerror($socket)."\n";
- 
-$bind = socket_bind($socket, $ip, $port); // 绑定一个SOCKET
-if ($bind)
-    echo "socket_bind() successed!\n";
-else
-    echo "socket_bind() failed:".socket_strerror($bind)."\n";
- 
-$listen = socket_listen($socket); // 间听SOCKET
-if ($listen)
-    echo "socket_listen() successed!\n";
-else
-    echo "socket_listen() failed:".socket_strerror($listen)."\n";
- 
-while (true) {
-    $msg = socket_accept($socket); // 接受一个SOCKET
-    if (!$msg) {
-        echo "socket_accept() failed:".socket_strerror($msg)."\n";
-        break;
-    }
-    $welcome = "Welcome to PHP Server!\n";
-    socket_write($msg, $welcome, strlen($welcome));
-    while (true) {
-        $command = strtoupper(trim(socket_read($msg, 1024)));
-        if (!$command)
-            break;
-        switch ($command) {
-            case "HELLO":
-                $writer = "Hello Everybody!";
-                break;
-            case "QUIT":
-                $writer = "Bye-Bye";
-                break;
-            case "HELP":
-                $writer = "HELLO\tQUIT\tHELP";
-                break;
-            default:
-                $writer = "Error Command!";
+    /**
+     * 构造函数
+     * @param string  $address 监听地址
+     * @param integer $port    监听端口
+     */
+    public function __construct($address = 'localhost', $port = 80)
+    {
+        $this->port = $port;
+        $this->address = $address;
+        $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if (! $this->socket) {
+            throw new Exception("Http Server create failed!");
         }
-        socket_write($msg, $writer, strlen($writer));
-        if ($command == "QUIT")
-            break;
+        //绑定地址和端口
+        socket_bind($this->socket, $address, $this->port);
     }
-    socket_close($msg);
-}
-socket_close($socket); // 关闭SOCKET
 
-?>
+    /**
+     * 析构函数
+     */
+    public function __destruct()
+    {
+        socket_close($this->socket);
+    }
+
+    /**
+     * 开始运行http服务器
+     */
+    public function run()
+    {
+        //开始进行监听
+        socket_listen($this->socket);
+        while (true) {
+            //获取请求socket
+            $msg_socket = socket_accept($this->socket);
+            //获取请求内容
+            $buf = socket_read($msg_socket, 99999);
+            echo $buf;  //输出请求内容
+            //写入相应内容（输出"Hello World!"）
+            socket_write($msg_socket, $this->text("Hello World!"));
+            //关闭请求socket
+            socket_close($msg_socket);
+        }
+    }
+
+    /**
+     * 获取http协议的文本内容
+     * @param  string $content string
+     * @return string
+     */
+    private function text($content)
+    {
+        //协议头
+        $text = 'HTTP/1.0 200 OK' . "\r\n";
+        $text .= 'Content-Type: text/plain' . "\r\n";
+        $text .= 'Content-Length: ' . strlen($content) . "\r\n";
+
+        //以空行分隔
+        $text .= "\r\n";
+
+        //协议正文
+        $text .= $content;
+        return $text;
+    }
+}
+
+//测试
+$server = new HttpServer();
+echo "Server running at http://localhost\r\n";
+$server->run();
